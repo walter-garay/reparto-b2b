39c6bb50d4ab000cd079e66ac5b46c75cbe7ef3d
David Garay <davidgaraycr@gmail.com>
2024-07-04 14:52:44 -0500

    avance

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,64 +1,64 @@
         </main>
         <footer class="footer">
-    <div class="footer-container">
-        <div class="footer-main">
-            <div class="footer-col footer-logo-col">
-                <img class="footer-logo" src="../../assets/img/LogoF.png" alt="Confi Courier Logo">
-                <p class="footer-description">
-                    Sistema integral de gestión de repartos para optimizar 
-                    tus entregas. Eficiencia, seguimiento en tiempo real y satisfacción garantizada.
-                </p>
-            </div>
-            <div class="footer-col">
-                <h3 class="footer-heading">Enlaces Rápidos</h3>
-                <ul class="footer-links">
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Zonas de Cobertura</a></li>
-                    <li><a href="#">Cotización</a></li>
-                    <li><a href="#">Comprobantes</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h3 class="footer-heading">Legal y Soporte</h3>
-                <ul class="footer-links">
-                    <li><a href="#">Términos y Condiciones</a></li>
-                    <li><a href="#">Política de Privacidad</a></li>
-                    <li><a href="#" class="book-complaints"><i class="fas fa-book"></i> Libro de Reclamaciones</a></li>
-                    <li><a href="#">Preguntas Frecuentes</a></li>
-                    <li><a href="#">Centro de Ayuda</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h3 class="footer-heading">Contacto</h3>
-                <ul class="footer-contact">
-                    <li><i class="fas fa-map-marker-alt"></i> Av. Principal 123, Lima, Perú</li>
-                    <li><i class="fas fa-phone"></i> (01) 234-5678</li>
-                    <li><i class="fas fa-envelope"></i> info@conficourier.com</li>
-                </ul>
-            </div>
-            <div class="footer-col footer-social-col">
-                <h3 class="footer-heading">Síguenos</h3>
-                <div class="footer-social-icons">
-                    <a href="https://www.facebook.com/" target="_blank" class="social-icon"><i class="fab fa-facebook-f"></i></a>
-                    <a href="https://www.instagram.com/" target="_blank" class="social-icon"><i class="fab fa-instagram"></i></a>
-                    <a href="https://twitter.com/" target="_blank" class="social-icon"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.youtube.com/" target="_blank" class="social-icon"><i class="fab fa-youtube"></i></a>
+            <div class="footer-container">
+                <div class="footer-main">
+                    <div class="footer-col footer-logo-col">
+                        <img class="footer-logo" src="../../assets/img/LogoF.png" alt="Confi Courier Logo">
+                        <p class="footer-description">
+                            Sistema integral de gestión de repartos para optimizar 
+                            tus entregas. Eficiencia, seguimiento en tiempo real y satisfacción garantizada.
+                        </p>
+                    </div>
+                    <div class="footer-col">
+                        <h3 class="footer-heading">Enlaces Rápidos</h3>
+                        <ul class="footer-links">
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="#">Servicios</a></li>
+                            <li><a href="#">Zonas de Cobertura</a></li>
+                            <li><a href="#">Cotización</a></li>
+                            <li><a href="#">Comprobantes</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h3 class="footer-heading">Legal y Soporte</h3>
+                        <ul class="footer-links">
+                            <li><a href="#">Términos y Condiciones</a></li>
+                            <li><a href="#">Política de Privacidad</a></li>
+                            <li><a href="#" class="book-complaints"><i class="fas fa-book"></i> Libro de Reclamaciones</a></li>
+                            <li><a href="#">Preguntas Frecuentes</a></li>
+                            <li><a href="#">Centro de Ayuda</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h3 class="footer-heading">Contacto</h3>
+                        <ul class="footer-contact">
+                            <li><i class="fas fa-map-marker-alt"></i> Av. Principal 123, Lima, Perú</li>
+                            <li><i class="fas fa-phone"></i> (01) 234-5678</li>
+                            <li><i class="fas fa-envelope"></i> info@conficourier.com</li>
+                        </ul>
+                    </div>
+                    <div class="footer-col footer-social-col">
+                        <h3 class="footer-heading">Síguenos</h3>
+                        <div class="footer-social-icons">
+                            <a href="https://www.facebook.com/" target="_blank" class="social-icon"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.instagram.com/" target="_blank" class="social-icon"><i class="fab fa-instagram"></i></a>
+                            <a href="https://twitter.com/" target="_blank" class="social-icon"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.youtube.com/" target="_blank" class="social-icon"><i class="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="footer-bottom">
-        <div class="footer-payment-methods">
-            <img src="../../assets/img/social/Visa.png" alt="Visa">
-            <img src="../../assets/img/social/Mastercard.png" alt="Mastercard">
-            <img src="../../assets/img/social/Express.png" alt="American Express">
-        </div>
-        <p class="footer-copyright">
-            &copy; 2024 Confi Courier. Todos los derechos reservados.
-        </p>
-    </div>
-    </footer>
+            <div class="footer-bottom">
+                <div class="footer-payment-methods">
+                    <img src="../../assets/img/social/Visa.png" alt="Visa">
+                    <img src="../../assets/img/social/Mastercard.png" alt="Mastercard">
+                    <img src="../../assets/img/social/Express.png" alt="American Express">
+                </div>
+                <p class="footer-copyright">
+                    &copy; 2024 Confi Courier. Todos los derechos reservados.
+                </p>
+            </div>
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
